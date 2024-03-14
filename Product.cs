6fde72af9362901c61057a266b47d294dd0cf978
nelson@wiretooth.com <nelson@wiretooth.com>
2024-03-14 17:48:39 +0100

    Override ToString in Product class

--- Product.cs
+++ Product.cs
@@ -5,4 +5,6 @@ public class Product
     public string Description { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
+
+    public override string ToString() => $"{Name}\n{Description}\n{Price}\n{Quantity}";
 }
