84e36d03b7efd5cb11c1d3e553b870df22e5c883
nelson@wiretooth.com <nelson@wiretooth.com>
2024-03-14 17:04:11 +0100

    Add user console interaction

--- Program.cs
+++ Program.cs
@@ -1,56 +1,47 @@
-﻿Console.WriteLine("\nInventory Management System");
-Console.WriteLine("\nChoose an option from the below options");
-Console.WriteLine("1. Add Product");
-Console.WriteLine("2. Remove Product");
-Console.WriteLine("3. Update Product");
-Console.WriteLine("4. Display All Products");
-Console.WriteLine("5. Exit");
+﻿IProductRepository productRepository = new ProductRepository();
+var inventoryManager = new InventoryManager(productRepository);
 
-int productOption;
-int.TryParse(Console.ReadLine(), out productOption);
+while (true)
+{
+    Console.WriteLine("\nInventory Management System");
+    Console.WriteLine("1. Add Product");
+    Console.WriteLine("2. Remove Product");
+    Console.WriteLine("3. Update Product");
+    Console.WriteLine("4. Display All Products");
+    Console.WriteLine("5. Exit");
 
+    Console.Write("Enter your choice: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
 
-// while (true)
-//         {
-//             Console.WriteLine("\nInventory Management System");
-//             Console.WriteLine("1. Add Product");
-//             Console.WriteLine("2. Remove Product");
-//             Console.WriteLine("3. Update Product");
-//             Console.WriteLine("4. Display All Products");
-//             Console.WriteLine("5. Exit");
-
-//             Console.Write("Enter your choice: ");
-//             int choice = Convert.ToInt32(Console.ReadLine());
-
-//             switch (choice)
-//             {
-//                 case 1:
-//                     // Add Product
-//                     Product newProduct = CreateProduct();
-//                     inventoryManager.AddProduct(newProduct);
-//                     break;
-//                 case 2:
-//                     // Remove Product
-//                     Console.Write("Enter Product ID to remove: ");
-//                     int removeProductID = Convert.ToInt32(Console.ReadLine());
-//                     inventoryManager.RemoveProduct(removeProductID);
-//                     break;
-//                 case 3:
-//                     // Update Product
-//                     Product updatedProduct = CreateProduct();
-//                     inventoryManager.UpdateProduct(updatedProduct);
-//                     break;
-//                 case 4:
-//                     // Display All Products
-//                     inventoryManager.DisplayAllProducts();
-//                     break;
-//                 case 5:
-//                     // Exit program
-//                     Console.WriteLine("Exiting...");
-//                     Environment.Exit(0);
-//                     break;
-//                 default:
-//                     Console.WriteLine("Invalid choice. Please try again.");
-//                     break;
-//             }
-//         }
+    switch (choice)
+    {
+        case 1:
+            // Add Product
+            Product newProduct = productRepository.CreateProduct();
+            inventoryManager.AddProduct(newProduct);
+            break;
+        case 2:
+            // Remove Product
+            Console.Write("Enter Product ID to remove: ");
+            int removeProductID = Convert.ToInt32(Console.ReadLine());
+            inventoryManager.RemoveProduct(removeProductID);
+            break;
+        case 3:
+            // Update Product
+            Product updatedProduct = productRepository.CreateProduct();
+            inventoryManager.UpdateProduct(updatedProduct);
+            break;
+        case 4:
+            // Display All Products
+            inventoryManager.GetAllProducts();
+            break;
+        case 5:
+            // Exit program
+            Console.WriteLine("Exiting...");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+    }
+}
